473af40f2fdac045053722a112b31f79401c2726
Gura <tuananhtran180904@gmail.com>
2024-08-20 18:11:13 +0700

    Adjust users table SQL
    add IF NOT EXIST to create table statement

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE users (
-                         id SERIAL PRIMARY KEY,
-                         name VARCHAR(20),
-                         password VARCHAR(255),
-                         email VARCHAR(255),
-                         phone VARCHAR(255),
-                         active BOOLEAN DEFAULT FALSE
-);
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    password VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    active BOOLEAN DEFAULT FALSE
+    );
